ce702d85dd75302026321f8c8b546ca954e03b02
Nic <nicolas.polum@outlook.com>
2026-04-15 01:10:03 -0400

    Body: drop vertical padding, keep horizontal only

--- src/lib/layout/Body.svelte
+++ src/lib/layout/Body.svelte
@@ -4,6 +4,6 @@
 	let { children }: { children: Snippet } = $props();
 </script>
 
-<div class="min-h-screen bg-background flex flex-col max-w-7xl p-4 mx-auto">
+<div class="min-h-screen bg-background flex flex-col max-w-7xl px-4 mx-auto">
 	{@render children()}
 </div>
